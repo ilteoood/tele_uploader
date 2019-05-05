67ddd09457cffbc1f1cad405c7d37d0242ce3ef3
iLTeoooD <matteopietro.dazzi@gmail.com>
2019-05-05 11:59:14 +0200

    Refactoring step 4, message dispatching

--- bot.php
+++ bot.php
@@ -16,33 +16,46 @@ if (!file_exists(TMP_DOWNLOADS))
 $MadelineProto = makeLogin();
 $offset = 0;
 $conversations = array();
+
 while (true) {
     $updates = $MadelineProto->get_updates(['offset' => $offset, 'limit' => 50, 'timeout' => 0]);
     foreach ($updates as $update) {
         $offset = $update['update_id'] + 1;
-        switch ($update['update']['_']) {
-            case 'updateNewMessage':
-            case 'updateNewChannelMessage':
-                if (isset($update['update']['message']['out']) && $update['update']['message']['out']) {
-                    continue;
+        manageSingleUpdate($update);
+    }
+}
+
+function manageSingleUpdate($update)
+{
+    global $conversations;
+    switch ($update['update']['_']) {
+        case 'updateNewMessage':
+        case 'updateNewChannelMessage':
+            if (isset($update['update']['message']['out']) && $update['update']['message']['out']) {
+                continue;
+            }
+            try {
+                if (isMediaIncoming($update)) {
+                    handleMediaMessage($update, $conversations);
+                } else if (isTextMessage($update)) {
+                    manageSingleTextMessage($update);
                 }
-                try {
-                    $destination = retrieveDestination($update);
-                    if (isMediaIncoming($update)) {
-                        handleMediaMessage($update, $conversations);
-                    } else if (isTextMessage($update)) {
-                        $message = retrieveFromMessage($update, 'message');
-                        if (isDownloadableFile($message)) {
-                            handleDownloadMessage($update);
-                        } else if ($message == '/dropbox') {
-                            handleDropboxMessage($update, $conversations);
-                        } else if ($message == '/telegram') {
-                            handleTelegramMessage($update, $conversations);
-                        }
-                    }
-                } catch (RPCErrorException $e) {
-                    sendMessage('@ilteoood', $e->getCode() . ': ' . $e->getMessage() . PHP_EOL . $e->getTraceAsString(), null);
-                }
-        }
+            } catch (RPCErrorException $e) {
+                sendMessage('@ilteoood', $e->getCode() . ': ' . $e->getMessage() . PHP_EOL . $e->getTraceAsString(), null);
+            }
+    }
+    return $update;
+}
+
+function manageSingleTextMessage($update)
+{
+    global $conversations;
+    $message = retrieveFromMessage($update, 'message');
+    if (isDownloadableFile($message)) {
+        handleDownloadMessage($update);
+    } else if ($message == '/dropbox') {
+        handleDropboxMessage($update, $conversations);
+    } else if ($message == '/telegram') {
+        handleTelegramMessage($update, $conversations);
     }
 }
